d5c1090f9ec4dd6645689e85895bc375e1c809fb
Moussa Ndou <msndourr@gmail.com>
2025-06-08 23:22:18 +0200

    add a new route called code/ for viewing project source.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template  # redirect, url_for
+from flask import Flask, render_template, request  # redirect, url_for
+from flask import Response
 import plotly
 import json
 
@@ -62,6 +63,50 @@ def backtest():
 @app.route("/description")
 def description():
     return render_template("description.html")
+
+
+# @app.route("/code")
+# def code_viewer():
+#     option = request.args.get("option", "strategy")  # Default to 'strategy'
+    
+#     code_map = {
+#         "strategy": "src/create_figures_strategy.py",
+#         "optimization": "src/create_figures_optimization.py",
+#         "backtesting": "src/create_figures_backtest.py"
+#     }
+    
+#     selected_file = code_map.get(option, code_map["strategy"])
+
+#     try:
+#         with open(selected_file, "r") as f:
+#             code_content = f.read()
+#     except FileNotFoundError:
+#         code_content = "# Error: File not found."
+
+#     return render_template("code.html", code=code_content, selected=option)
+
+@app.route("/code")
+def code_home():
+    return render_template("code.html")
+
+
+@app.route("/code/<section>")
+def serve_code(section):
+    files = {
+        "strategy": "src/strategy_performance.py",
+        "optimization": "src/portfolio_optimization.py",
+        "backtesting": "src/backtest_optimizer.py"
+    }
+
+    if section not in files:
+        return Response("Invalid section", status=404)
+
+    try:
+        with open(files[section], "r") as f:
+            code = f.read()
+        return Response(code, mimetype="text/plain")
+    except FileNotFoundError:
+        return Response("Code file not found", status=404)
 
 
 if __name__ == "__main__":
